24b711c65c8a4bf679c70c85e52431dff1c811bf
Damian Sobolewski <damiansobolewski12@gmail.com>
2019-01-28 00:33:22 +0100

    ranking

---
+++ src/game/Ranking.java
@@ -0,0 +1,28 @@
+package game;
+
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+
+import java.io.IOException;
+
+public class Ranking {
+    static String parse(String nick, int score) {
+        Document document = null;
+        try {
+            document = Jsoup.connect("http://przypomnienia.cba.pl/javaAPP/ranking.php?nick=" + nick + "&score=" + score).timeout(1000).get();
+        } catch (IOException e) {
+            System.out.println("Brak internetu");
+        }
+
+        String result = "";
+        if (document != null) {
+            for (Element actualElement : document.select("tr")) {
+                result += actualElement.select("td#score").text();
+                result += " - ";
+                result += actualElement.select("td#nick").text();
+                result += "\n";
+            }
+        }
+        return result;
+    }
+}
